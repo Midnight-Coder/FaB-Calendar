a50a6a9b1d2164f4ac2757d8d56aafe8513fed0d
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2015-04-26 00:54:41 -0700

    Adds functions to club together overlapping events and tests

--- js/main.js
+++ js/main.js
@@ -1,4 +1,34 @@
 var sampleList = [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670}];
-(function layOutDay(events) {
-    //
-}(sampleList));
+function layOutDay(events) {
+    var markup, $div;
+    //Sort events by start time
+    events.sort(function(a,b){
+        return a.start - b.start;
+    });
+    //Group all overlapping events together
+    events = resolveConflicts(events);
+
+    //Wrap the width corresponding to each event
+    evalWidth(events);
+}
+function resolveConflicts(list){
+    //Group all overlapping events together
+    var resolvedList = [],
+        overlap = [list[0]];
+    for(var i=1; i<list.length; i++){
+        if(list[i-1].end < list[i].start){
+            //Separate non overlapping event from the previous list of overlapping events
+            resolvedList.push(overlap);
+            overlap = [list[i]];
+        }
+        else{
+            //Else club together with the other overlapping events in that timeframe
+            overlap.push(list[i]);
+        }
+    }
+    resolvedList.push(overlap);
+    return resolvedList;
+}
+function evalWidth(list){
+
+}
--- test/main-spec.js
+++ test/main-spec.js
@@ -1,6 +1,26 @@
 var suite = this;
-describe('Index JS', function(){
-    it('should work', function(){
-        expect(true).toBe(true);
+describe('Fab Cal', function(){
+
+    it('should take an unsorted list and sort it wrt start time', function(){
+        var unsortedList = [{start: 30, end: 150}, {start: 560, end: 620}, {start: 540, end: 600}],
+            sortedList = [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}];
+        suite.layOutDay(unsortedList);
+        expect(unsortedList).toEqual(sortedList);
+    });
+
+    it('should be able to group overlapping transactions into the same list', function(){
+        var conflictingList = [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 620}],
+            resolvedList = [[{start: 30, end: 150}], [{start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 620}]];
+
+        expect(suite.resolveConflicts(conflictingList)).toEqual(resolvedList);
+    });
+
+    it('should be able to attach the width of the event card UI', function(){
+        var list = [[{start: 30, end: 150}],
+                    [{start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 620}]],
+            eventsWithWidth = [[{start: 30, end: 150, width: 90}],
+                    [{start: 540, end: 600, width: 30}, {start: 560, end: 620, width: 30}, {start: 610, end: 620, width: 30}]];
+        suite.evalWidth(list);
+        expect(list).toEqual(eventsWithWidth);
     });
 });
