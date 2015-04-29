9df77ad6b1c180f63469a7f875eeb02023dbe650
Sagar Jhobalia <sagar.jhobalia@gmail.com>
2015-04-28 22:14:58 -0700

    Adds a globabl map for storing maping dimensions for event cards Introduces absolute positioning of cards and calculates left for positioning overlaping cards

--- js/main.js
+++ js/main.js
@@ -1,7 +1,24 @@
 var sampleList = [{start: 30, end: 150}, {start: 540, end: 600}, {start: 560, end: 620}, {start: 610, end: 670}];
-$(document).ready(function(){
+                //{9:30 am - 11:30 am}, {6:00 pm - 7:00 pm}, {6:20 pm - 7:20 pm}, {7:10 pm - 8:10 pm}
+/*
+Methods for Measurements:
+    height & margin-top: defines the units 'px' since time events are specified in px.
+    height: defines the height/length of the event card per 30min slot
+    marginTop: defines the margin of the card from the top of the container
+
+    width & left: units '%' relative to container width
+    widht: defines the width/breadth of the event card (varies with number of overlaping cards)
+    left: defines the position of the card on the X-axis (for overlaping events)
+*/
+var eventCardDimensions = {
+    height: 26,
+    width: 93,
+    marginTop: 24,
+    left: 32
+};
+window.onload = function(){
     layOutDay(sampleList);
-});
+};
 function layOutDay(events) {
     var markupFragment, $div;
     //Sort events by start time
@@ -45,35 +62,38 @@ function resolveConflicts(list){
     return resolvedList;
 }
 function evalWidth(nestedList){
-    var k = 90 * 100,   //*100 -> optimize for rounding to second decimal place
+    var k = eventCardDimensions.width * 100,   //*100 -> optimize for rounding to second decimal place
         width;
     nestedList.forEach(function(groupOfEvents){
         //Each event in the group has to have the same width which totals to W (W=90% based on our styles)
         //Round to the nearest hundredth
         width = (Math.ceil(k/groupOfEvents.length))/100;
-        for(var i = 0; i < groupOfEvents.length; i++){
+        groupOfEvents[0].width = width;
+        groupOfEvents[0].left = 0;
+        for(var i = 1; i < groupOfEvents.length; i++){
             groupOfEvents[i].width = width;
+            groupOfEvents[i].left = eventCardDimensions.left + groupOfEvents[i-1].left;
         }
     });
 }
 function evalMarginTop(list){
-    var k = 6.3 * 100,   //*100 -> optimize for rounding to second decimal place
+    var k = eventCardDimensions.marginTop * 10,   //*10 -> optimization -> precalc of constants
         marginTop;
     list.forEach(function(entry){
-        //Eval marginTop as the distance for 9:00 AM (00). Every 30 min slot = 6.3% margin-top
+        //Eval marginTop as the distance for 9:00 AM (00)
         marginTop = (entry.start*k)/30;
-        //Round to the nearest hundredth
-        entry.marginTop = Math.ceil(marginTop)/100;
+        //Round to the nearest tenth (px is accurate for 1 decimal notation)
+        entry.marginTop = Math.ceil(marginTop)/10;
     });
 }
 function evalHeight(list){
     var height;
     list.forEach(function(entry){
-        //Eval height as the distance from (end-start). Every 30 min slot = 10% height
+        //Eval height as the distance from (end-start)
 
         //Round to nearest tens
         height = (Math.round((entry.end - entry.start)/10))*10
-        height = (height)/3;  //--> optimized (height*10)/30 to (height/3)
+        height = (height*eventCardDimensions.height)/30;
         //Round to the nearest hundredth
         entry.height = Math.round(height);
     });
@@ -101,8 +121,9 @@ function makeCalendarEventCards(nestedList){
             $clonedNode = $div.cloneNode(true);
             $clonedNode.children[0].innerHTML +=  nestedList[i][j].start + '-' + nestedList[i][j].end;
             $clonedNode.style.width = nestedList[i][j].width+'%';
-            $clonedNode.style.height = nestedList[i][j].height+'%';
-            $clonedNode.style.marginTop = nestedList[i][j].marginTop+'%';
+            $clonedNode.style.height = nestedList[i][j].height+'px';
+            $clonedNode.style.left = nestedList[i][j].left+'%';
+            $clonedNode.style.marginTop = nestedList[i][j].marginTop+'px';
             eventCards.appendChild($clonedNode);
         }
     }
